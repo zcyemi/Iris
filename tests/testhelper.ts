06635e4bb3a51aacd2f93478eebfd1cf29625b07
Yemi <zcyemi@gmail.com>
2018-12-19 12:39:22 +0800

    Fix tests.

--- tests/testhelper.ts
+++ tests/testhelper.ts
@@ -1,5 +1,5 @@
 import * as chai from 'chai';
-import { vec3, vec4, quat, mat4 } from 'wglut'
+import { mat4, vec3, vec4, quat } from '../src/math/GLMath';
 const expect = chai.expect;
 
 export function pairwise(src:Array<any>,f:(s:any,t:any)=>void,dest:Array<any>){
@@ -30,3 +30,5 @@ export function expectVec4(v1:vec4,v2:vec4,torenlance:number = 0.001){
 export function expectQuat(q1:quat,q2:quat){
     expect(q1.equals(q2)).to.eq(true,`${q1.raw} , ${q2.raw}`);
 }
+
+
