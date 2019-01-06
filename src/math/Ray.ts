168f83f48a0e50c056f8c7ed2a4ecc4da649b46a
yemi <zcyemi@gmail.com>
2019-01-07 01:20:02 +0800

    Add ray

---
+++ src/math/Ray.ts
@@ -0,0 +1,30 @@
+import { vec3, f32 } from "./GLMath";
+
+
+export class Ray{
+    public origin:vec3;
+    public direction:vec3;
+
+    public constructor(origin?:vec3,dir?:vec3){
+        this.origin = origin != null? origin: vec3.zero;
+        this.direction = dir != null ? dir: vec3.forward;
+    }
+
+    public getPoint(d:number):vec3{
+        return this.direction.mulNumToRef(d).add(this.origin);
+    }
+
+    public distantToPointSq(p:vec3){
+        let toPoint = p.subToRef(this.origin);
+        let cos = toPoint.dot(this.direction);
+        return toPoint.length2 - cos * cos;
+    }
+
+    public distantToPoint(p:vec3){
+        return Math.sqrt(this.distantToPointSq(p));
+    }
+
+    public sphereIntersect(pos:vec3,radius:f32):boolean{
+        return this.distantToPointSq(pos) < radius * radius;
+    }
+}
