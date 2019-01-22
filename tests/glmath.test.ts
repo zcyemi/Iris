ddb14df350fa6f759f24b09fe46aa7893a5eccc6
yemi <zcyemi@gmail.com>
2019-01-22 15:59:27 +0800

    migrate test files.

---
+++ tests/glmath.test.ts
@@ -0,0 +1,46 @@
+import * as chai from 'chai';
+import { expectVec3, expectQuat, expectPair, expectMat4 } from './testhelper';
+import { mat3, quat, vec3, mat4, vec4 } from '../src/math/GLMath';
+
+const expect = chai.expect;
+
+
+
+describe("glmath", () => {
+    it("colne-mat3", () => {
+        let mtx = mat3.fromRS(quat.Random(), vec3.Random());
+        let raw = mtx.raw.slice(0);
+        let mtxc = mtx.clone();
+        let rawclone = mtxc.raw;
+        expectPair(raw, rawclone);
+        expectPair(mtx.raw, rawclone);
+        expect(mtx.raw).not.eq(mtxc.raw);
+    });
+    it("colne-mat4", () => {
+        let mtx = mat4.TRS(vec3.Random(), quat.Random(), vec3.Random());
+        let raw = mtx.raw.slice(0);
+        let mtxc = mtx.clone();
+        let rawclone = mtxc.raw;
+        expectPair(raw, rawclone);
+        expectPair(mtx.raw, rawclone);
+        expect(mtx.raw).not.eq(mtxc.raw);
+    });
+    it("colne-vec3", () => {
+        let v = vec3.Random();
+        let vc = v.clone();
+        expectPair(v.raw, vc.raw);
+        expect(v.raw).not.eq(vc.raw);
+    });
+    it("colne-vec4", () => {
+        let v = vec4.Random();
+        let vc = v.clone();
+        expectPair(v.raw, vc.raw);
+        expect(v.raw).not.eq(vc.raw);
+    });
+    it("colne-quat", () => {
+        let v = quat.Random();
+        let vc = v.clone();
+        expectPair(v.raw, vc.raw);
+        expect(v.raw).not.eq(vc.raw);
+    });
+})
