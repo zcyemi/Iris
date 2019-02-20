b118a28133d7c795f814847e583c38f30ef7ed80
yemi <zcyemi@gmail.com>
2019-02-20 23:16:59 +0800

    Add IndexedTypedBuffer.

--- src/collection/IndexedBuffer.ts
+++ src/collection/IndexedBuffer.ts
@@ -1,5 +1,3 @@
-
-
 export class IndexedBuffer<T>{
 
     public array:T[] = [];
@@ -25,5 +23,56 @@ export class IndexedBuffer<T>{
         this.array = [];
         this.size = 0;
         this.capacity = 0;
+    }
+}
+
+export type TypedArray = Float32Array | Int32Array | Uint8Array | Uint16Array | Int16Array |Int8Array | Uint32Array;
+export class IndexedTypedBuffer<T extends TypedArray>{
+    public array:T;
+    private m_capacity:number;
+    public size:number = 0;
+
+    private m_type:new(size:number)=>T;
+
+    public get capacity():number{return this.m_capacity;}
+
+    public constructor(type:new(size:number)=>T,size:number = 128){
+        this.array = new type(size);
+        this.m_capacity = size;
+        this.m_type = type;
+    }
+    
+    public push(val:any){
+        if(this.size >= this.m_capacity){
+            this.extendArray();
+        }
+        this.array[this.size] = val;
+        this.size++;
+    }
+
+    public checkExten(newsize:number):boolean{
+        if(this.size+newsize >= this.m_capacity){
+            this.extendArray();
+            return true;
+        }
+        return false;
+    }
+
+    private extendArray(){
+        let capa = this.m_capacity * 2;
+        this.m_capacity = capa;
+        let newary = new this.m_type(capa);
+        newary.set(this.array,0);
+        this.array = newary;
+    }
+
+    public empty(){
+        this.size = 0;
+    }
+
+    public release(){
+        this.array = null;
+        this.m_capacity = 0;
+        this.size = 0;
     }
 }
